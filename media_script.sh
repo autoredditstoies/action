0cb3c861bf2ec2641aceebeaf2c13c5062ad8285
autoredditstoies <113731879+autoredditstoies@users.noreply.github.com>
2022-09-21 00:24:33 -0700

    Update media_script.sh

--- media_script.sh
+++ media_script.sh
@@ -6,7 +6,7 @@ cd temp
 git clone https://autoredditstoies:${TOKEN}@${MEDIA_GENERATOR} .
 cd media-generator
 chmod +x run.sh
-./run.sh > temp.txt
-git add . > temp.txt
-git commit -m "Done" > temp.txt
-git push origin main > temp.txt
+./run.sh > log.txt 2>&1
+git add . >>log.txt 2>&1
+git commit -m "Done" >>log.txt 2>&1
+git push origin main >>log.txt 2>&1
