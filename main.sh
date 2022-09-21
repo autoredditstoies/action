2208f54426f93da8b645d13ce3549be4095ccd16
autoredditstoies <113731879+autoredditstoies@users.noreply.github.com>
2022-09-21 08:47:51 -0700

    try 3

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 echo "Running script..."
 mkdir temp
 cd temp
-git clone https://autoredditstoies:${TOKEN}@${REPO} .
+git clone https://autoredditstoies:${TOKEN}@${REPO} && cd "$(basename "$_" .git)"
 cd $REPO
 npm run start > log.txt 2>&1
 git add . >>log.txt 2>&1
