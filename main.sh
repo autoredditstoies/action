42518f04657adbf3a9eb98d0fd7793951111bf0c
autoredditstoies <113731879+autoredditstoies@users.noreply.github.com>
2022-09-21 08:38:22 -0700

    run start

--- main.sh
+++ main.sh
@@ -5,8 +5,7 @@ mkdir temp
 cd temp
 git clone https://autoredditstoies:${TOKEN}@${REPO} .
 cd ${REPO}
-chmod +x run.sh
-./run.sh > log.txt 2>&1
+npm run start > log.txt 2>&1
 git add . >>log.txt 2>&1
 git commit -m "Update" >>log.txt 2>&1
 git push origin >>log.txt 2>&1
