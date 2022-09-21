37e642c651a0312a39402e23b332d8062d9d5a35
autoredditstoies <113731879+autoredditstoies@users.noreply.github.com>
2022-09-21 14:51:09 -0700

    Update media_script.sh

--- media_script.sh
+++ media_script.sh
@@ -6,6 +6,9 @@ cd temp
 git clone https://autoredditstoies:${TOKEN}@${MEDIA_GENERATOR} .
 chmod +x run.sh
 ./run.sh
+
+git config --global user.email ${EMAIL}
+git config --global user.name "autoredditstories"
 git add .
 git commit -m "Done"
 git push origin main
