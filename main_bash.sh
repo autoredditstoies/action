54c3e9fcff77253a260814c16f1833f08a5c1b02
autoredditstoies <113731879+autoredditstoies@users.noreply.github.com>
2022-09-22 05:47:53 -0700

    add user info

--- main_bash.sh
+++ main_bash.sh
@@ -8,6 +8,9 @@ git clone https://autoredditstoies:${TOKEN}@${REPO} .
 chmod +x run.sh
 ./run.sh
 
+git config --global user.email ${EMAIL} > temp.txt 2>&1
+git config --global user.name "autoredditstories" > temp.txt 2>&1
+
 git add .
 git commit -m "Done"
 git push origin
