74986b4d9043b98aa031ac83e6b70f0055ed5038
autoredditstoies <113731879+autoredditstoies@users.noreply.github.com>
2023-05-26 09:36:49 -0700

    Update media_script.sh

--- media_script.sh
+++ media_script.sh
@@ -5,8 +5,7 @@ cd temp
 
 git clone https://autoredditstoies:${TOKEN}@${MEDIA_GENERATOR} .
 chmod +x run.sh
-# ./run.sh > log.txt 2>&1
-./run.sh
+./run.sh > log.txt 2>&1
 
 git config --global user.email ${EMAIL} > temp.txt 2>&1
 git config --global user.name "autoredditstories" > temp.txt 2>&1
